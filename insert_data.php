ecaabc72b69285a1f7947d67b527a6a2f5f2f29d
Ferdi ali <bissmillah97@gmail.com>
2024-07-04 01:07:30 +0800

    setting database

--- insert_data.php
+++ insert_data.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "algoritma";
+$password = "Algoritma97";
 $dbname = "db_keluhan";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
