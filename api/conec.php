7f3d7471115991cba3261692954b103636b4557d
unknown <ferdi ali>
2025-04-04 08:24:49 +0800

    edit tanggal

--- api/conec.php
+++ api/conec.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "localhost";
-// $username_db = "u545755515_algoritma";
-// $password_db = "Algoritma97";
-// $dbname = "u545755515_db_keluhan";
-
 $servername = "localhost";
-$username_db = "root";
-$password_db = "";
-$dbname = "db_keluhan";
+$username_db = "u545755515_algoritma";
+$password_db = "Algoritma97";
+$dbname = "u545755515_db_keluhan";
+
+// $servername = "localhost";
+// $username_db = "root";
+// $password_db = "";
+// $dbname = "db_keluhan";
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username_db, $password_db, $dbname);
